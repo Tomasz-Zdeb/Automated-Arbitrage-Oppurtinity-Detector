907322f150606687d72e9e11498d188af9bb0ced
Tomasz Zdeb <62356875+Tomasz-Zdeb@users.noreply.github.com>
2024-05-06 20:26:18 +0200

    Improve scheduler error handling

--- Uniswap/src/services/SchedulerService.ts
+++ Uniswap/src/services/SchedulerService.ts
@@ -5,6 +5,7 @@ import cron from 'node-cron';
 import { ScheduledTask } from 'node-cron';
 import { Config } from '../models/Config';
 import { IScheduler } from '../interfaces/IScheduler'
+import { SchedulerError } from '../models/SchedulerError'
 
 
 export class SchedulerService implements IScheduler {
@@ -21,46 +22,57 @@ export class SchedulerService implements IScheduler {
     }
 
     public addJob(action: () => Promise<void>): void {
-
-
+      try {
         const wrappedAction = async () => {
-            let timeoutHandle: NodeJS.Timeout | null = null;
-      
-            const timeoutPromise = new Promise((_, reject) => {
-              timeoutHandle = setTimeout(() => {
-                reject(this.logger.error(`Task timeout (${this.config.schedulerTimeout}ms) reached, stopping the task`));
-              }, this.config.schedulerTimeout);
-            });
-      
-            try {
-              await Promise.race([action(), timeoutPromise]);
-            } catch (error) {
-                this.logger.error('Error occured during task execution:', error);
-            } finally {
-                if (timeoutHandle) {
-                    clearTimeout(timeoutHandle);
-                }
-            }
-          };
-
+          let timeoutHandle: NodeJS.Timeout | null = null;
+    
+          const timeoutPromise = new Promise((_, reject) => {
+            timeoutHandle = setTimeout(() => {
+              reject(this.logger.error(`Task timeout (${this.config.schedulerTimeout}ms) reached, stopping the task`));
+            }, this.config.schedulerTimeout);
+          });
+    
+          try {
+            await Promise.race([action(), timeoutPromise]);
+          } catch (error) {
+              this.logger.error(`Error occured during task execution: ${error}`);
+          } finally {
+              if (timeoutHandle) {
+                  clearTimeout(timeoutHandle);
+              }
+          }
+        };
 
         const task = cron.schedule(this.config.schedulerConfig, wrappedAction, {
           scheduled: true,
           timezone: this.timezone
         });
+        
         this.tasks.push(task);
-        this.logger.debug(`Cron job added and scheduled to run at: ${this.config.schedulerConfig} with timeout ${this.config.schedulerTimeout}ms, timezone set to: ${this.timezone}`);
+      } catch(e) {
+          throw new SchedulerError(`SchedulerService.addJob() Error ${e}`)
+      }
+      this.logger.debug(`Cron job added and scheduled to run at: ${this.config.schedulerConfig} with timeout ${this.config.schedulerTimeout}ms, timezone set to: ${this.timezone}`);
     }
 
     public startAllJobs(): void {
         this.logger.debug("Scheduler starting jobs");
-        this.tasks.forEach(task => task.start());
+        try {
+          this.tasks.forEach(task => task.start());
+        } catch (e) {
+          throw new SchedulerError(`SchedulerService.startAllJobs() Error: ${e}`)
+        }
+        
         this.logger.info('All scheduler jobs started');
     }
     
     public stopAllJobs(): void {
         this.logger.debug("Scheduler stopping jobs");
-        this.tasks.forEach(task => task.stop());
+        try {
+          this.tasks.forEach(task => task.stop());
+        } catch (e) {
+          throw new SchedulerError(`SchedulerService.stopAllJobs() Error ${e}`)
+        }
         this.logger.info('All scheduler jobs stopped');
     }
 }
