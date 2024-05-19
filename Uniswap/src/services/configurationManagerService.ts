cef09c9638c961b497962d8d5d87170be68f22da
Tomasz Zdeb <62356875+Tomasz-Zdeb@users.noreply.github.com>
2024-05-19 13:11:19 +0200

    Deletete unnecessary comments

--- Uniswap/src/services/configurationManagerService.ts
+++ Uniswap/src/services/configurationManagerService.ts
@@ -9,11 +9,7 @@ export class ConfigurationManagerService implements IConfigurationSource {
     commandLineParser?: IConfigurationSource
   ) {
     this.configuration = fileConfigurationProvider.getConfiguration();
-    //TODO IMPLEMENT CONDITIONAL FETCHING FOR COMMAND LINE PARSER IF IT WAS PASSED TO CTOR
   }
-
-  //IMPLEMENT PRIVATE METHOD THAT WILL BE INVOKED IN CONSTRUCTOR THAT OVERRIDES/ADDSCONFIG
-  //FROM COMMAND LINE.
 
   getConfiguration(): Config{
     return this.configuration;
